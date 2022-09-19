51887176abe4bca69e738e1c0cb041dc1289b944
Jerod Merritt <jerodmerritt@gmail.com>
2022-09-19 01:14:55 -0500

    More player movement updates

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,24 +5,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D player;
-    private float JumpValue = 14f;
-    private float MoveValue = 4f;
+    private SpriteRenderer playerSprite;
+    private Animator playerAnimation;
+    private float jumpValue = 14f;
+    private float moveValue = 4f;
 
     // Start is called before the first frame update
     private void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+        playerAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
-        player.velocity = new Vector2(dirX * MoveValue, player.velocity.y);
+        player.velocity = new Vector2(dirX * moveValue, player.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            player.velocity = new Vector2(player.velocity.x, JumpValue);
+            player.velocity = new Vector2(player.velocity.x, jumpValue);
+        }
+
+        UpdateAnimationState(dirX);
+    }
+
+    private void UpdateAnimationState(float direction)
+    {
+        if (direction > 0f)
+        {
+            playerAnimation.SetBool("running", true);
+            playerSprite.flipX = false;
+        }
+        else if (direction < 0f)
+        {
+            playerAnimation.SetBool("running", true);
+            playerSprite.flipX = true;
+        }
+        else 
+        {
+            playerAnimation.SetBool("running", false);
         }
     }
 }
