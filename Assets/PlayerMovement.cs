d80f8073e18b7d5ca879bfeba8eadf32e47538bb
Jerod Merritt <jerodmerritt@gmail.com>
2022-09-19 00:36:39 -0500

    Added basic player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D player;
+    private float JumpValue = 14f;
+    private float MoveValue = 4f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float dirX = Input.GetAxisRaw("Horizontal");
+        player.velocity = new Vector2(dirX * MoveValue, player.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            player.velocity = new Vector2(player.velocity.x, JumpValue);
+        }
     }
 }
