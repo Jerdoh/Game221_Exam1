dab36f907bfacafa6c563ec47921e087db80c9ee
Jerod Merritt <jerodmerritt@gmail.com>
2022-09-24 00:06:38 -0500

    Added death animation and level restart on death.

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D player;
+    private Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        player.bodyType = RigidbodyType2D.Static;
+        playerAnimator.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
